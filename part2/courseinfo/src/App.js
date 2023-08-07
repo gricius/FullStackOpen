aad09e33b08f0e8910b37eb9fc21ed9bca2757f2
gricius <gricius@gmail.com>
2023-08-07 12:03:58 +0200

    add 2.1: Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 //App.js
+
 const Header = (props) => {
   console.log(props);
   return (
@@ -27,18 +28,26 @@ const Content = (props) => {
     </div>
   );
 };
-
-const Total = (props) => {
-  console.log(props);
-  const totalExercises = props.course.parts.reduce((acc, part) => acc + part.exercises, 0);
+const Course = ({course}) => {
   return (
     <div>
-      <p>
-        Number of exercises {totalExercises}
-      </p>
+      <Header course={course} />
+      <Content course={course} />
     </div>
   );
 };
+
+// const Total = (props) => {
+//   console.log(props);
+//   const totalExercises = props.course.parts.reduce((acc, part) => acc + part.exercises, 0);
+//   return (
+//     <div>
+//       <p>
+//         Number of exercises {totalExercises}
+//       </p>
+//     </div>
+//   );
+// };
 
 const App = () => {
   const course = {
@@ -61,9 +70,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   );
 };
