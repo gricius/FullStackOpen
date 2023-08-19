9be626ba64ab44ffe8dadca8b7d48474b231fc8e
gricius <gricius@gmail.com>
2023-08-19 14:13:05 +0200

    add Footer and Notification

--- additional/notes/src/App.js
+++ additional/notes/src/App.js
@@ -1,11 +1,15 @@
 import {useState, useEffect} from 'react';
 import Note from './components/Note';
 import noteService from './services/notes';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
 
   useEffect(() => {
     console.log('effect');
@@ -46,7 +50,10 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote));
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(`Note '${note.content}' was already removed from server`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter(n => n.id !== id));
       });
   };
@@ -63,6 +70,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -83,6 +91,7 @@ const App = () => {
           onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
